d941eeb545351c23a65d1914e0fa92b28e12e7c5
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-06-03 20:19:52 +0500

    footer

--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <>
+    <div className={`${styles.wrapper} wrapper`}>
       <Header page="contacts"/>
-      <motion.div className={styles.container} initial="pageInitial" animate="pageAnimate" variants={{
+      <motion.div className={`${styles.container} main`} initial="pageInitial" animate="pageAnimate" variants={{
         pageInitial: {
           opacity: 0
         },
@@ -22,6 +23,7 @@ export default function Home() {
         </Head>
         <h1>Contacts</h1>
       </motion.div>
-    </>
+      <Footer />
+    </div>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
-    <>
+    <div className={`${styles.wrapper} wrapper`}>
       <Header page="home"/>
-      <motion.div className={styles.container} initial="pageInitial" animate="pageAnimate" variants={{
+      <motion.div className={`${styles.container} main`} initial="pageInitial" animate="pageAnimate" variants={{
         pageInitial: {
           opacity: 0
         },
@@ -20,8 +21,9 @@ export default function Home() {
           <meta name="description" content="Shahriyorweb Full-stack developer" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <h1>Micheal Weaver</h1>
+        <h1>Home</h1>
       </motion.div>
-    </>
+      <Footer />
+    </div>
   )
 }
