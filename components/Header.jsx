6f13bd6c45e7d33b9bb40a83ff899835e7c200d4
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-06-03 18:26:24 +0500

    header

---
+++ components/Header.jsx
@@ -0,0 +1,136 @@
+import styles from '../styles/Header.module.scss'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+const Header = ({page}) => {
+  return (
+    <header className={styles.header}>
+        <div className={styles.left}>
+            <Link href="/">
+                <a className={`${styles.link} ${styles.logo}`}>shahriyor-web
+                    <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                    }} className={styles.border}></motion.div>
+                </a>
+            </Link>
+            {page == "home" ? (
+                <Link href="/">
+                    <a className={`${styles.link} ${styles.active}`}>_hello
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            ) : (
+                <Link href="/">
+                    <a className={`${styles.link}`}>_hello
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            )}
+            {page == "about" ? (
+                <Link href="/about">
+                    <a className={`${styles.link} ${styles.active}`}>_about-me
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            ) : (
+                <Link href="/about">
+                    <a className={`${styles.link}`}>_about-me
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            )}
+            {page == "projects" ? (
+                <Link href="/projects">
+                    <a className={`${styles.link} ${styles.active}`}>_projects
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            ) : (
+                <Link href="/projects">
+                    <a className={`${styles.link}`}>_projects
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            )}
+        </div>
+        <div className={styles.right}>
+            {page == "contacts" ? (
+                <Link href="/contacts">
+                    <a className={`${styles.link} ${styles.active}`}>_contacts
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            ) : (
+                <Link href="/contacts">
+                    <a className={`${styles.link}`}>_contacts
+                        <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+                            pageInitial: {
+                                opacity: 0
+                            },
+                            pageAnimate: {
+                                opacity: 1
+                            }
+                        }} className={styles.border}></motion.div>
+                    </a>
+                </Link>
+            )}
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ pages/about.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+import Header from '../components/Header'
+import { motion } from 'framer-motion'
+
+export default function Home() {
+  return (
+    <>
+      <Header page="about"/>
+      <motion.div className={styles.container} initial="pageInitial" animate="pageAnimate" variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        }
+      }}>
+        <Head>
+          <title>Shahriyorweb Full-stack developer</title>
+          <meta name="description" content="Shahriyorweb Full-stack developer" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>About</h1>
+      </motion.div>
+    </>
+  )
+}
