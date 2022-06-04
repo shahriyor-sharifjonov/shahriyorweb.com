0a7d31b29122f9bea1b5dbebe970faa04dded117
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-06-04 10:53:29 +0500

    contacts sidebar

---
+++ components/FindMeAccordion.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import styles from '../styles/Accordion.module.scss'
+
+const ContactsAccordion = ({id}) => {
+    const [isOpen, setIsOpen] = useState(true);
+    return (
+        <div className={styles.accordion}>
+            <motion.div onClick={()=>setIsOpen((prev) => !prev)} className={`${styles.button} ${isOpen ? styles.active : ''} ${styles.border}`}>
+                <svg width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.24998 6.58521L-3.10421e-05 0.585206L8.5 0.585205L4.24998 6.58521Z" fill="white"/>
+                </svg>
+                find-me-also-in 
+            </motion.div>
+            <AnimatePresence>
+                {isOpen ? (
+                    <motion.div 
+                        initial={{ opacity: 0, y: '-10%' }} animate={{
+                            opacity: 1,
+                            y: 0,
+                            transition: {
+                                duration: 0.5,
+                            },
+                        }}
+                        exit={{
+                            opacity: 0,
+                            y: '-10%',
+                            transition: {
+                                duration: 0.5,
+                            },
+                        }}
+                    >
+                        <a href="https://www.instagram.com/shahriyorweb/" rel="noreferrer" target="_blank" className={styles.link}>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.22222 2.66667V4.44444H1.77778V14.2222H11.5556V9.77778H13.3333V15.1111C13.3333 15.3469 13.2397 15.573 13.073 15.7397C12.9063 15.9064 12.6802 16 12.4444 16H0.888889C0.653141 16 0.427048 15.9064 0.260349 15.7397C0.0936505 15.573 0 15.3469 0 15.1111V3.55556C0 3.31981 0.0936505 3.09372 0.260349 2.92702C0.427048 2.76032 0.653141 2.66667 0.888889 2.66667H6.22222ZM16 0V7.11111H14.2222V3.03378L7.29511 9.96178L6.03822 8.70489L12.9644 1.77778H8.88889V0H16Z" />
+                            </svg>
+                            Instagram
+                        </a>
+                        <a href="https://t.me/shahriyorweb/" rel="noreferrer" target="_blank" className={styles.link}>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.22222 2.66667V4.44444H1.77778V14.2222H11.5556V9.77778H13.3333V15.1111C13.3333 15.3469 13.2397 15.573 13.073 15.7397C12.9063 15.9064 12.6802 16 12.4444 16H0.888889C0.653141 16 0.427048 15.9064 0.260349 15.7397C0.0936505 15.573 0 15.3469 0 15.1111V3.55556C0 3.31981 0.0936505 3.09372 0.260349 2.92702C0.427048 2.76032 0.653141 2.66667 0.888889 2.66667H6.22222ZM16 0V7.11111H14.2222V3.03378L7.29511 9.96178L6.03822 8.70489L12.9644 1.77778H8.88889V0H16Z" />
+                            </svg>
+                            Telegram
+                        </a>
+                        <a href="https://github.com/shahriyor-sharifjonov" rel="noreferrer" target="_blank" className={styles.link}>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.22222 2.66667V4.44444H1.77778V14.2222H11.5556V9.77778H13.3333V15.1111C13.3333 15.3469 13.2397 15.573 13.073 15.7397C12.9063 15.9064 12.6802 16 12.4444 16H0.888889C0.653141 16 0.427048 15.9064 0.260349 15.7397C0.0936505 15.573 0 15.3469 0 15.1111V3.55556C0 3.31981 0.0936505 3.09372 0.260349 2.92702C0.427048 2.76032 0.653141 2.66667 0.888889 2.66667H6.22222ZM16 0V7.11111H14.2222V3.03378L7.29511 9.96178L6.03822 8.70489L12.9644 1.77778H8.88889V0H16Z" />
+                            </svg>
+                            Github
+                        </a>
+                        <a href="https://kwork.ru/user/shahriyorweb/" rel="noreferrer" target="_blank" className={styles.link}>
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.22222 2.66667V4.44444H1.77778V14.2222H11.5556V9.77778H13.3333V15.1111C13.3333 15.3469 13.2397 15.573 13.073 15.7397C12.9063 15.9064 12.6802 16 12.4444 16H0.888889C0.653141 16 0.427048 15.9064 0.260349 15.7397C0.0936505 15.573 0 15.3469 0 15.1111V3.55556C0 3.31981 0.0936505 3.09372 0.260349 2.92702C0.427048 2.76032 0.653141 2.66667 0.888889 2.66667H6.22222ZM16 0V7.11111H14.2222V3.03378L7.29511 9.96178L6.03822 8.70489L12.9644 1.77778H8.88889V0H16Z" />
+                            </svg>
+                            Kwork
+                        </a>
+                    </motion.div>
+                ) : ('')}
+            </AnimatePresence>
+        </div>
+    )
+}
+
+export default ContactsAccordion
--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import styles from '../styles/Contacts.module.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion';
 import SubHeaderItem from '../components/SubHeaderItem'
 import { useState, useEffect } from 'react'
+import ContactsAccordion from '../components/ContactsAccordion';
+import FindMeAccordion from '../components/FindMeAccordion';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
@@ -61,7 +63,8 @@ export default function Contacts() {
       <Header page="contacts"/>
       <motion.div className={`${styles.container} main inner`} initial="pageInitial" animate="pageAnimate" variants={{pageInitial: {opacity: 0},pageAnimate: {opacity: 1}}}>
         <aside className={`${styles.sidebar} sidebar`}>
-
+          <ContactsAccordion />
+          <FindMeAccordion />
         </aside>
         <div className={`${styles.content} content`}>
           <div className={`subheader`}>
@@ -76,7 +79,12 @@ export default function Contacts() {
                 <input autoComplete='off' id="email" className={styles.input} type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <label htmlFor="message" className={`${styles.label}`}>_message:</label>
                 <textarea autoComplete='off' id="message" className={`${styles.input} ${styles.textarea}`} value={message} onChange={(e) => setMessage(e.target.value)} required />
-                <button type="submit" className={`btn ${styles.btn}`}>submit-message</button>
+                <motion.button type="submit" className={`btn ${styles.btn}`}
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  submit-message
+                </motion.button>
               </div>
             </div>
             <div className={`${styles.result}`}>
